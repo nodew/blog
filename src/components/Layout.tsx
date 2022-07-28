d742f097c8555b4701c155df0fad7fc8aea83b00
Qiao Wang <wangqiao11@hotmail.com>
2022-07-28 23:34:10 +0800

    Fix deploy issue

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,18 +10,10 @@ export interface LayoutProps {
     activeNavItem?: string;
 }
 
-declare var __PATH_PREFIX__: string;
-
 export const Layout = ({ children, activeNavItem }: LayoutProps) => {
-    const rootPath = `${__PATH_PREFIX__}/`;
-    const isRootPath = location.pathname === rootPath;
-
     return (
         <ThemeProvider>
-            <div
-                data-is-root-path={isRootPath}
-                className="bg-white dark:bg-dark dark:antialiased text-base text-gray-700 dark:text-gray-200 p-8 min-h-screen flex flex-col"
-            >
+            <div className="bg-white dark:bg-dark dark:antialiased text-base text-gray-700 dark:text-gray-200 p-8 min-h-screen flex flex-col">
                 <Header activeNavItem={activeNavItem} />
                 <div className="flex-1">{children}</div>
                 <Footer />
